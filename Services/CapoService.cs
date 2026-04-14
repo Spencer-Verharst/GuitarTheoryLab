a939a1d83b9ef0d7bfb491da143c2dc712e2c636
Spencer Verharst <spencev23@yahoo.com>
2026-04-14 13:21:01 -0500

    Add CapoService with capo calculation logic.

---
+++ Services/CapoService.cs
@@ -0,0 +1,36 @@
+namespace GuitarChordExplorer.Services;
+
+using GuitarChordExplorer.Models;
+
+public class CapoService
+{
+    private static readonly string[] Chromatic =
+        ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
+
+    private static readonly string[] OpenKeys = ["E", "A", "D", "G", "C"];
+
+    public CapoInfo GetCapo(string targetKey)
+    {
+        int targetIdx = Array.IndexOf(Chromatic, targetKey);
+
+        string bestGuitarKey = OpenKeys[0];
+        int bestFret = 12;
+
+        foreach(var openKey in OpenKeys)
+        {
+            int openIdx = Array.IndexOf(Chromatic, openKey);
+            int fret = (targetIdx - openIdx + 12) % 12;
+
+            if(fret < bestFret)
+            {
+                bestFret = fret;
+                bestGuitarKey = openKey;
+            }
+
+            return new CapoInfo
+            {
+                
+            }
+        }
+    }
+}
