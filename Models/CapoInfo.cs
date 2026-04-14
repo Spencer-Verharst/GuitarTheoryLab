6f50727a52fb4fada19d15132789de05bf549e30
Spencer Verharst <spencev23@yahoo.com>
2026-04-14 10:47:02 -0500

    Add CapoInfo Model Content

---
+++ Models/CapoInfo.cs
@@ -0,0 +1,8 @@
+namespace GuitarChordExplorer.Models;
+
+public class CapoInfo
+{
+    public required string TargetKey { get; init; }
+    public required int CapoFret { get; init; }
+    public required string GuitarKey { get; init; }
+}
